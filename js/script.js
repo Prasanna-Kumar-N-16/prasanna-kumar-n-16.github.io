5669b09da4d60254ea217ad9b3e03e04361d73f5
Prasanna-Kumar-Nagaboyina-16 <radhaprasanna77@gmail.com>
2024-06-02 21:11:15 -0500

    changes to script.js

--- js/script.js
+++ js/script.js
@@ -1,47 +1,43 @@
 /*================ toggle bar ===============*/
 
-let nemuIcon = document.querySelector('#menu-icon');
+let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
-menuIcon.onclick = () => {
+menuIcon.addEventListener('click', () => {
     menuIcon.classList.toggle('fa-xmark'); 
     navbar.classList.toggle('active')
-}
+});
 
 /*================ scroll section bar ===============*/
 
-let sections=document.querySelectorAll('section');
-let navLinks=document.querySelectorAll('header nav a');
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 
-window.onscroll = ()=>{
+window.onscroll = () => {
     sections.forEach(sec => {
-let top=window.scrollY;
-let offset=sec.offsetTop - 150;
-let height=sec.offsetHeight;
-let id=sec.getAttribute('id');
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
 
-if(top >= offset && top < offset + height) {
-navLinks.forEach.apply(links => {
-links.classList.remove('active');
- document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-});
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
 
-};
+    /*=============== sticky navbar =============*/
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
 
-});
-/*=============== sticky navbar =============*/
-
-let header=document.querySelector('header');
-header.classList.toggle('sticky', window.scrollY > 100);
-
-/*===== remove toggle icon and navbar ===*/
-
-menuIcon.classList.remove('fa-xmark');
-navbar.classList.remove('active');
+    /*===== remove toggle icon and navbar ===*/
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
 };
 
 /*===== scroll reveal =============*/
-
 ScrollReveal({
     distance: '80px',
     duration: 2000,
